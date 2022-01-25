71eebb5d451ded6aa4d6c0accfb94b4da852fe54
Martin Villavicencio <villavicencio.martin@protonmail.com>
2022-01-25 16:09:01 +0100

    forgot something

--- longcontext/transformers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-    This module contains the class for the LongFormerWithLMHead
-    as the current LongFormer model does not support
-    autoregressive language modeling
-"""
-from transformers import LongFormer
