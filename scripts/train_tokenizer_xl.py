a4d3d6faa1488528800bdfbb8973b4e413ec913f
Villavicencio Martin <villavicencio.martin@protonmail.com>
2022-01-26 21:44:54 +0100

    training tokenizer-xl on wikitext2

--- scripts/train_tokenizer_xl.py
+++ scripts/train_tokenizer_xl.py
@@ -1,4 +1,3 @@
-from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.processors import TemplateProcessing
 from transformers import TransfoXLTokenizer
 
@@ -23,28 +22,10 @@ def main():
     tokenizer.mask_token = "<msk>"
 
     for split in ["test", "train", "valid"]:
-        tokenizer.vocab_file = f"./data/wikitext-2/wiki.{split}.tokens"
-        tokenizer.build_vocab()
+        tokenizer.count_file(f"./data/wikitext-2/wiki.{split}.tokens", True)
+    tokenizer.build_vocab()
 
-    # Split by whitespace
-    tokenizer.pre_tokenizer = Whitespace()
-
-    # Train on wikitext-2
-    files = [f"./data/wikitext-2/wiki.{split}.tokens" for split in ["test", "train", "valid"]]
-
-    # Add start and end sequence tag at beginning/end
-    tokenizer.post_processor = TemplateProcessing(
-        single="<s> $A </s>", # As far as I know we don't use sentence pairs
-        special_tokens=[
-            ("<s>", tokenizer.token_to_id("<s>")),
-            ("</s>", tokenizer.token_to_id("</s>"))
-        ]
-    )
-
-    # Add padding token
-    tokenizer.pad_token = tokenizer.token_to_id("<pad>")
-
-    tokenizer.save("data/tokenizer-wiki2.json")
+    tokenizer.save_pretrained("data/tokenizer-xl-wiki2.json")
 
 
 if __name__ == "__main__":
