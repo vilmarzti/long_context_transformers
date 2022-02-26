5a50697ed4ac339c4331f8444972912066dde36a
Villavicencio Martin <villavicencio.martin@protonmail.com>
2022-02-26 17:21:01 +0100

    fixed lr_schedule

--- scripts/transformers/train_transformer_xl.py
+++ scripts/transformers/train_transformer_xl.py
@@ -1,6 +1,6 @@
 import torch
 
-from transformers import get_scheduler, AdamW, TransfoXLConfig, TransfoXLLMHeadModel, TransfoXLTokenizer
+from transformers import get_scheduler, TransfoXLConfig, TransfoXLLMHeadModel, TransfoXLTokenizer
 from longcontext.utils.dataset import get_dataloader
 from longcontext.utils.training import train
 
@@ -8,13 +8,15 @@ from longcontext.utils.training import train
 def main():
     epochs = 30
     max_length = 32
+    batch_size = 10
+    samples = 1024
 
     # Get tokenizer
     tokenizer = TransfoXLTokenizer.from_pretrained("data/tokenizer-xl-wiki2")
     tokenizer.model_max_length = max_length
 
     # Get Dataloaders processed by TransfoXLTokenizer
-    train_loader, valid_loader, _ = get_dataloader(tokenizer, samples=2048, batch_size=10, max_length=max_length, valid_samples=128)
+    train_loader, valid_loader, _ = get_dataloader(tokenizer, samples=samples, batch_size=10, max_length=max_length, valid_samples=128)
 
     # Create Model
     config = TransfoXLConfig(
@@ -32,10 +34,10 @@ def main():
     # Set optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 
-    #lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=5, num_training_steps=epochs)
+    lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=5, num_training_steps=epochs*samples/batch_size)
 
     # train
-    train(model, train_loader, optimizer, epochs, valid_loader, device=device, subsequence_len=max_length)#, lr_scheduler=lr_scheduler)
+    train(model, train_loader, optimizer, epochs, valid_loader, device=device, subsequence_len=max_length, lr_scheduler=lr_scheduler)
     
 
 if __name__ == "__main__":
